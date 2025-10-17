c0b0c35fbdf153c3ba41aea83efde9d1a5fbb572
cristianb1983 <crisbenegas83@gmail.com>
2025-10-17 07:34:30 -0300

    clase proyeccion

---
+++ src/entidades/Proyeccion.java
@@ -0,0 +1,15 @@
+package entidades;
+
+import java.time.LocalTime;
+
+
+public class Proyeccion {
+private Pelicula pelicula;
+private String idioma;
+private boolean es3D;
+private boolean subtitulada;
+private LocalTime horaInicio;
+private LocalTime horaFin;
+private Sala sala;
+private double precio;
+}
