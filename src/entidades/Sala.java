7966b2e290227d522023514eaaeaee095ac972d7
Ayelen Guadalupe Aguero <agueroayelen10@gmail.com>
2025-10-23 20:41:03 -0300

    Cambios en la clase Sala

--- src/entidades/Sala.java
+++ src/entidades/Sala.java
@@ -2,10 +2,14 @@ package entidades;
 
 
 public class Sala {
-private int NroSala;
-private boolean apta3D;
-private int capacidad;
-private boolean estado;
+    private int idSala = -1;    
+    private int NroSala;
+    private boolean apta3D;
+    private int capacidad;
+    private boolean estado;
+
+    public Sala() {
+    }
 
     public Sala(int NroSala, boolean apta3D, int capacidad, boolean estado) {
         this.NroSala = NroSala;
@@ -14,9 +18,12 @@ private boolean estado;
         this.estado = estado;
     }
 
-   
+    public int getIdSala() {
+        return idSala;
+    }
 
-    public Sala() {
+    public void setIdSala(int idSala) {
+        this.idSala = idSala;
     }
 
     public int getNroSala() {
@@ -25,6 +32,14 @@ private boolean estado;
 
     public void setNroSala(int NroSala) {
         this.NroSala = NroSala;
+    }
+
+    public boolean isApta3D() {
+        return apta3D;
+    }
+
+    public void setApta3D(boolean apta3D) {
+        this.apta3D = apta3D;
     }
 
     public int getCapacidad() {
@@ -43,6 +58,8 @@ private boolean estado;
         this.estado = estado;
     }
 
+    
+    
     @Override
     public String toString() {
         return "Sala{" + "NroSala=" + NroSala + ", capacidad=" + capacidad + ", estado=" + estado + '}';
